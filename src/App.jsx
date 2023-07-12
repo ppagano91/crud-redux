e2303796b755b8b4c34d40087ef95276e5960c5a
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-12 09:52:27 -0300

    Agregando React Router DOM y Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import "./App.css";
 import React, { Fragment } from "react";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Header from "./components/Header";
+import Productos from "./components/Productos";
+import NuevoProducto from "./components/NuevoProducto";
+import EditarProducto from "./components/EditarProducto";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-    </>
+      <div className="container">
+        <Routes>
+          <Route exact path="/" element={<Productos />} />
+          <Route exact path="/productos/nuevo" element={<NuevoProducto />} />
+          <Route
+            exact
+            path="/productos/editar/:id"
+            element={<EditarProducto />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
