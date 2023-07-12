ff20e27e6d797efe83d042f73ddce4558604aef9
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-12 09:13:25 -0300

    Creando componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import React, { Fragment } from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1>REDUX</h1>
+      <Header />
     </>
   );
 }
