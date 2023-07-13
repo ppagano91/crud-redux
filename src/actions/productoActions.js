9ae3731b79081a57a2cb2d215f18b746f236f032
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-13 09:50:09 -0300

    Agregando un Nuevo Producto al State

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -6,7 +6,29 @@ import {
 
 // Crear nuevos productos
 export function crearNuevoProductoAction(producto) {
-  return () => {
-    console.log(producto);
+  return (dispatch) => {
+    dispatch(agregarProducto());
+    try {
+      dispatch(agregarProductoExito(producto));
+    } catch (error) {
+      dispatch(agregarProductoError(true));
+    }
   };
 }
+
+const agregarProducto = () => ({
+  type: AGREGAR_PRODUCTO,
+  payload: true,
+});
+
+// Si el producto se guarda en la base de datos
+const agregarProductoExito = (producto) => ({
+  type: AGREGAR_PRODUCTO_EXITO,
+  payload: producto,
+});
+
+// Si hubo un error
+const agregarProductoError = (estado) => ({
+  type: AGREGAR_PRODUCTO_ERROR,
+  payload: estado,
+});
