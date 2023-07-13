d61a9a6823298df4c23d3429bc4309ee58ed7412
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-13 10:04:09 -0300

    Mostrando los errores

--- src/components/NuevoProducto.jsx
+++ src/components/NuevoProducto.jsx
@@ -12,6 +12,10 @@ const NuevoProducto = () => {
   // Utilizar use dispatch y te crea una funcion
   const dispatch = useDispatch();
 
+  // Acceder al state del store
+  const cargando = useSelector((state) => state?.productos?.loading);
+  const error = useSelector((state) => state?.productos?.error);
+
   // Mandar llamar el action de productoAction con un dispatch
   const agregarProducto = (producto) =>
     dispatch(crearNuevoProductoAction(producto));
@@ -69,6 +73,12 @@ const NuevoProducto = () => {
                 Agregar
               </button>
             </form>
+            {cargando ? <p>Cargando...</p> : null}
+            {error ? (
+              <p className="alert alert-danger p2 mt-4 text-center">
+                Hubo un error
+              </p>
+            ) : null}
           </div>
         </div>
       </div>
